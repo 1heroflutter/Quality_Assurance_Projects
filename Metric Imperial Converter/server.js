ec7ad2085b62fffbf3a2cfb68f57d1b4d2af3d5a
meninheaven <vonhat10a4nh1@gmail.com>
2025-10-23 22:21:23 +0700

    Cap Nhat server.js

--- Metric Imperial Converter/server.js
+++ Metric Imperial Converter/server.js
@@ -3,7 +3,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require('dotenv').config({ path: __dirname + '/sample.env' });
+require('dotenv').config();
+
 
 const apiRoutes = require('./routes/api.js');
 const fccTestingRoutes = require('./routes/fcctesting.js');
