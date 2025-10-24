4c12ab6e543f0138c4d97f66dfae4ad0a299d079
meninheaven <vonhat10a4nh1@gmail.com>
2025-10-24 11:06:46 +0700

    Cap nhat Metric Imperial Converter

--- Metric Imperial Converter/controllers/convertHandler.js
+++ Metric Imperial Converter/controllers/convertHandler.js
@@ -51,24 +51,25 @@ function ConvertHandler() {
     return map[unit];
   };
 
-  this.convert = function(initNum, initUnit) {
-    const galToL = 3.78541;
-    const lbsToKg = 0.453592;
-    const miToKm = 1.60934;
-    let result;
+this.convert = function(initNum, initUnit) {
+  const galToL = 3.78541;
+  const lbsToKg = 0.453592;
+  const miToKm = 1.60934;
+  let result;
+  const unit = initUnit.toLowerCase();
 
-    switch (initUnit) {
-      case 'gal': result = initNum * galToL; break;
-      case 'L': result = initNum / galToL; break;
-      case 'lbs': result = initNum * lbsToKg; break;
-      case 'kg': result = initNum / lbsToKg; break;
-      case 'mi': result = initNum * miToKm; break;
-      case 'km': result = initNum / miToKm; break;
-      default: return 'invalid unit';
-    }
+  switch (unit) {
+    case 'gal': result = initNum * galToL; break;
+    case 'l': result = initNum / galToL; break;   
+    case 'lbs': result = initNum * lbsToKg; break;
+    case 'kg': result = initNum / lbsToKg; break;
+    case 'mi': result = initNum * miToKm; break;
+    case 'km': result = initNum / miToKm; break;
+    default: return 'invalid unit';
+  }
 
-    return parseFloat(result.toFixed(5));
-  };
+  return parseFloat(result.toFixed(5)); // 
+};
 
   this.getString = function(initNum, initUnit, returnNum, returnUnit) {
     return `${initNum} ${this.spellOutUnit(initUnit)} converts to ${returnNum} ${this.spellOutUnit(returnUnit)}`;
